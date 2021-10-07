6d629284d158ad65ec4d13394c17cfc5be5ffa6c
LaboratorInc <90879946+LaboratorInc@users.noreply.github.com>
2021-10-06 20:22:18 -0400

    Create source.lua

---
+++ src/source.lua
@@ -0,0 +1,12 @@
+loadstring("http://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source", true)
+loadstring("http://raw.githubusercontent.com/LaboratorInc/Steel/master/src/version.lua", true)
+
+local Plugin = {
+	["PluginName"] = "Steel " + version,
+	["PluginDescription"] = "Steel",
+	["Commands"] = {
+		
+	}
+}
+
+Title.Text = "Steel " + version
