3ff7113b163d05b10c4b9e1e302967ceca0cb2fd
LaboratorInc <90879946+LaboratorInc@users.noreply.github.com>
2021-10-07 21:33:16 -0400

    Update source.lua

--- src/source.lua
+++ src/source.lua
@@ -1,12 +1,4 @@
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"), true))()
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/LaboratorInc/Steel/master/src/version.lua"), true))()
 
-local Plugin = {
-	["PluginName"] = "Steel " + version,
-	["PluginDescription"] = "Steel",
-	["Commands"] = {
-		
-	}
-}
-
 Title.Text = "Steel " + version
